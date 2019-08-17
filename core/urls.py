a5ef5cc1d46f78395e168deefc8d6583febc91e7
Yannick KIKI <seyive.kiki@gmail.com>
2019-08-17 12:24:41 +0100

    Make menu links available

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view())
+    path('', views.HomeView.as_view(), name='home'),
+    path('about', views.AboutView.as_view(), name='about'),
+    path('career', views.CareerView.as_view(), name='career'),
+    path('blog', views.BlogView.as_view(), name='blog')
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,15 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "core/home.html"
+
+
+class AboutView(TemplateView):
+    template_name = "core/about.html"
+
+
+class CareerView(TemplateView):
+    template_name = "core/career.html"
+
+
+class BlogView(TemplateView):
+    template_name = "core/blog.html"
